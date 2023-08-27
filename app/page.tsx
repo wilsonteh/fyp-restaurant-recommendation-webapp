0d367adfa2a56ed63f44de1767199a54f80dd384
Wilson <ywteh13@gmail.com>
2023-08-27 15:50:37 +0800

    Customize next-ui theme color for light and dark mode
    1. Theme colors: primary, secondary, success, warning, danger

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,13 @@
+import { Button } from "@nextui-org/button"
+
 export default function Home() {
   return (
     <main>
-      hello
+      <Button color="primary">Primary</Button>
+      <Button color="secondary">Secondary</Button>
+      <Button color="success">Success</Button>
+      <Button color="warning">Warning</Button>
+      <Button color="danger">Danger</Button>
     </main>
   )
 }
