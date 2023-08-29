e02b5b23fe8efe165601a72343920d8f928e0525
Wilson <ywteh13@gmail.com>
2023-08-29 23:26:35 +0800

    Connect firebase to the project with the config file

---
+++ firebase/config.ts
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDkNgPO5j7_SDpqt26yt6W9SAuVRs8pDL4",
+  authDomain: "fyp-restaurant-recommendation.firebaseapp.com",
+  projectId: "fyp-restaurant-recommendation",
+  storageBucket: "fyp-restaurant-recommendation.appspot.com",
+  messagingSenderId: "306108806861",
+  appId: "1:306108806861:web:290ed1ebed605698bd5b84"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
