59c45fdde81bda2646d860ff2518cf0e8a35434b
Wilson <ywteh13@gmail.com>
2023-10-14 18:06:16 +0800

    Make route API for fetching Place Detail

---
+++ app/api/place-detail/route.ts
@@ -0,0 +1,23 @@
+import { NextResponse } from "next/server";
+
+const BASE_URL = "https://maps.googleapis.com/maps/api/place/details/json";
+const API_KEY = process.env.GOOGLE_CLOUD_API_KEY!;
+
+export async function GET(request: Request) {
+  console.log("--- 😓😓😓 Calling place detail API --- ");
+
+  const { searchParams } = new URL(request.url);
+  const placeId = searchParams.get("placeId");
+
+  const res = await fetch (
+    `${BASE_URL}?key=${API_KEY}&place_id=${placeId}`, 
+    { 
+      headers: {
+        'Content-Type': 'application/json'
+      },
+    }
+  );
+  const data = await res.json();
+  
+  return NextResponse.json(data);
+};
--- app/restaurant/[restaurantId]/page.tsx
+++ app/restaurant/[restaurantId]/page.tsx
@@ -1,10 +1,24 @@
 import RestaurantDetail from "@/components/RestaurantDetail";
 
-export default function RestaurantDetailPage({
+async function fetchRestaurant(placeId: string) {
+  const res = await fetch(`${process.env.HOST_URL}/api/place-detail?placeId=${placeId}`);
+
+  if (!res.ok) {
+    throw new Error(`Failed to fetch restaurant of id: ${placeId}`);
+  }
+
+  return res.json();
+}
+
+export default async function RestaurantDetailPage({
   params,
 }: {
   params: { restaurantId: string }
 }) {
+
+  const restaurant = await fetchRestaurant(params.restaurantId);
+  console.log("🚀 ~ file: page.tsx:20 ~ restaurant:", restaurant)
+
   return (
     <main className="max-w-screen-xl mx-auto h-screen border-red-500">
       restaurant id: { params.restaurantId }  
