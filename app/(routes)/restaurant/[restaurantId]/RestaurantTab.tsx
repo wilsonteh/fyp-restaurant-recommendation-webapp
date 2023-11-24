2ee9c9cf68b3bb72d7a4a7c9afb83bff2e3309f7
Wilson <ywteh13@gmail.com>
2023-11-24 23:32:21 +0800

    Improve the representation of features & serves in `DetailsTab` with icons
    1. Apply better styling on Tabs

--- app/(routes)/restaurant/[restaurantId]/RestaurantTab.tsx
+++ app/(routes)/restaurant/[restaurantId]/RestaurantTab.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { BellConcierge, CircleInfo, DollarSign, Heart, LocationDot, Pen, Utensils } from "@/app/_icons/Index";
+import { Beer, BellConcierge, Breakfast, CalendarAlt, Check, CircleInfo, DeliveryDining, Dinner, DollarSign, Heart, Leaf, LocationDot, Lunch, Pen, TakeoutDining, Times, Utensils, Wheelchair, WineGlassAlt } from "@/app/_icons/Index";
 import CircleChevronRightIcon from "@/app/_icons/circle-chevron-right";
 import { RestaurantDetailInterface } from "@/app/_utils/interfaces/PlaceDetailInterface";
 import { Button, Tab, Tabs } from "@nextui-org/react";
@@ -20,25 +20,23 @@ export default function RestaurantTab ({
   return (
     <Tabs
       aria-label="Tab options"
-      color="primary"
-      variant="solid"
+      variant="underlined"
       fullWidth={true}
       selectedKey={selectedTab}
       onSelectionChange={(key) => setSelectedTab(key as string)}
       classNames={{
-        tabList: "bg-gray-200 p-2",
-        tab: "py-2 h-full",
+        tabList: "bg-slate-200 rounded-full",
+        tab: "py-2 h-full rounded-full",
       }}
     >
       <Tab
         key="details"
         title={
-          <div className="flex items-center space-x-2">
+          <div className="flex items-center gap-2 text-slate-700 text-base">
             <CircleInfo size={15} />
             <span>Details</span>
           </div>
         }
-        className="w-full"
       >
         <DetailsTab 
           restaurant={restaurant} 
@@ -49,7 +47,7 @@ export default function RestaurantTab ({
       <Tab
         key="location"
         title={
-          <div className="flex items-center space-x-2">
+          <div className="flex items-center gap-2 text-slate-700 text-base">
             <LocationDot size={15} />
             <span>Location</span>
           </div>
@@ -61,7 +59,7 @@ export default function RestaurantTab ({
       <Tab
         key="reviews"
         title={
-          <div className="flex items-center space-x-2">
+          <div className="flex items-center gap-2 text-slate-700 text-base">
             <Pen size={15} />
             <span>Reviews</span>
           </div>
@@ -88,20 +86,19 @@ const DetailsTab = ({
     { label: "atmosphere", value: 4.3, icon: <Heart size={15} /> },
   ];
   const features = [
-    { key: "delivery", icon: "" },
-    { key: "dine_in", icon: "" },
-    { key: "takeout", icon: "" },
-    { key: "reservable", icon: "" },
-    { key: "wheelchair_accessible_entrance", icon: "" },
+    { key: "delivery", icon: <DeliveryDining size={18} /> },
+    { key: "dine_in", icon: <Utensils size={18} /> },
+    { key: "takeout", icon: <TakeoutDining size={18}/> },
+    { key: "reservable", icon: <CalendarAlt size={18} /> },
+    { key: "wheelchair_accessible_entrance", icon: <Wheelchair size={18} /> },
   ];
-
   const serves = [
-    { key: "serves_beer", icon: "" },
-    { key: "serves_wine", icon: "" },
-    { key: "serves_breakfast", icon: "" },
-    { key: "serves_lunch", icon: "" },
-    { key: "serves_dinner", icon: "" },
-    { key: "serves_vegetarian_food", icon: "" },
+    { key: "serves_beer", icon: <Beer size={15} /> },
+    { key: "serves_wine", icon: <WineGlassAlt size={15} /> },
+    { key: "serves_breakfast", icon: <Breakfast size={15} /> },
+    { key: "serves_lunch", icon:  <Lunch size={15} />},
+    { key: "serves_dinner", icon: <Dinner size={15} /> },
+    { key: "serves_vegetarian_food", icon: <Leaf size={15} /> },
   ];
 
   const OpeningHours = weekday_text.map((text) => {
@@ -116,14 +113,14 @@ const DetailsTab = ({
 
   return (
     <div className="grid grid-cols-2 gap-4">
-      <div className="opening-hour px-8 py-4 bg-slate-200/40 rounded-lg flex flex-col gap-1">
-        <h4 className="text-lg font-medium">Opening hours</h4>
+      <div className="opening-hour px-8 py-4 bg-slate-200/40 rounded-lg flex flex-col gap-3">
+        <h4 className="text-lg font-medium capitalize text-center">Opening hours</h4>
         <ul className="flex flex-col gap-1 text-sm"> {OpeningHours} </ul>
       </div>
 
-      <div className="multi-rating px-8 py-4 bg-zinc-200/30 rounded-lg flex flex-col gap-1">
-        <h4 className="text-lg font-medium">Rating by component</h4>
-        <div className="flex flex-col gap-3 text-sm">
+      <div className="multi-rating px-8 py-4 bg-zinc-200/30 rounded-lg flex flex-col gap-3">
+        <h4 className="text-lg font-medium  capitalize text-center">Rating by component</h4>
+        <div className="flex flex-col items-stretch gap-3 text-sm">
           {ratings.map(({ label, value, icon }) => (
             <div key={label} className="flex justify-between items-center capitalize">
               <div className="flex items-center gap-5">
@@ -137,7 +134,7 @@ const DetailsTab = ({
           <Button
             size="sm"
             endContent={<CircleChevronRightIcon size={12} />}
-            className="bg-gray-800 text-primary-400"
+            className="bg-gray-800 text-primary-400 self-end px-6 py-2"
             onClick={() => setSelectedTab("reviews")}
           >
             View Reviews
@@ -145,39 +142,39 @@ const DetailsTab = ({
         </div>
       </div>
 
-      <div className="features px-8 py-4 bg-zinc-200/30 rounded-lg flex flex-col gap-1">
-        <h3 className="text-lg font-medium">Features</h3>
+      <div className="features px-8 py-4 bg-zinc-200/30 rounded-lg flex flex-col gap-3">
+        <h3 className="text-lg font-medium capitalize text-center">Features</h3>
         {features.map(({ key, icon }) => (
-          <div
-            key={key}
-            className="flex justify-between items-center gap-x-2 capitalize text-sm"
-          >
-            {icon}
-            <span className="flex-initial w-[150px]">
-              {" "}
-              {key.replace("_", " ")}{" "}
+          <div key={key} className="flex justify-between items-center capitalize text-sm">
+            <div className="flex justify-center items-center gap-4">
+              <span> {icon} </span>
+              <span> {key.replaceAll("_", " ")} </span>
+            </div>
+            <span> 
+              {restaurant[key] 
+              ? <Check size={15} className="text-success-700" /> 
+              : <Times size={15} className="text-danger-600" /> } 
             </span>
-            <span> {restaurant[key] ? "Yes" : "No"} </span>
           </div>
         ))}
       </div>
 
-      <div className="serve px-8 py-4 bg-slate-200/40 rounded-lg flex flex-col gap-1">
-        <h3 className="text-lg font-medium">Serves</h3>
+      <div className="serve px-8 py-4 bg-slate-200/40 rounded-lg flex flex-col gap-3">
+        <h3 className="text-lg font-medium capitalize text-center">Serves</h3>
         {serves.map(({ key, icon }) => (
-          <div
-            key={key}
-            className="flex justify-between items-center gap-x-2 capitalize text-sm"
-          >
-            {icon}
-            <span className="flex-initial w-[150px]">
-              {" "}
-              {key.replaceAll("_", " ")}{" "}
+          <div key={key} className="flex justify-between items-center capitalize text-sm">
+            <div className="flex justify-center items-center gap-4">
+              <span> {icon} </span>
+              <span>{key.replaceAll("_", " ")} </span>
+            </div>
+            <span> 
+              {restaurant[key] 
+              ? <Check size={15} className="text-success-700" /> 
+              : <Times size={15} className="text-danger-600" /> } 
             </span>
-            <span> {restaurant[key] ? "Yes" : "No"} </span>
           </div>
         ))}
       </div>
     </div>
   );
-}
+};
---
+++ app/_icons/takeout-dining.tsx
@@ -0,0 +1,31 @@
+import { IconProps } from "../_utils/interfaces/Interfaces";
+
+const TakeoutDiningIcon: React.FC<IconProps> = (props) => {
+  const { size, className, ...restProps } = props;
+
+  return (
+    <span className={className}>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        enable-background="new 0 0 24 24"
+        viewBox="0 0 24 24"
+        fill="currentColor"
+        width={size}
+        height={size}
+        {...restProps}
+      >
+        <g>
+          <rect fill="none" height="24" width="24" />
+        </g>
+        <g>
+          <path
+            d="M5.26,11h13.48l-0.67,9H5.93L5.26,11z M9.02,4h5.95L19,7.38l1.59-1.59L22,7.21 L19.21,10H4.79L2,7.21l1.41-1.41L5,7.38L9.02,4z"
+            fill-rule="evenodd"
+          />
+        </g>
+      </svg>
+    </span>
+  );
+};
+
+export default TakeoutDiningIcon;
