0b8ae80bae19c6607e7474ebbbb3fcbb5fbea13a
Wilson <ywteh13@gmail.com>
2023-11-27 15:28:13 +0800

    forgot to add to prev commit

--- app/_lib/data-fetching.ts
+++ app/_lib/data-fetching.ts
@@ -1,23 +1,5 @@
 import { RestaurantDetailInterface } from "../_utils/interfaces/PlaceDetailInterface";
 
-export async function fetchRestaurants(params: any) {
-  try {
-    const { lat, lng, radius } = params
-    const requestUrl = `${process.env.HOST_URL}/api/nearby-search?` + 
-      `lat=${lat}&` +
-      `lng=${lng}&` +
-      `radius=${radius}`;
-    
-    const res = await fetch(requestUrl);
-    const data = await res.json();
-    return data;
-
-  } catch (e) {
-    console.error("Error fetching restaurants", e);
-    throw new Error("Error fetching restaurants");
-  }
-};
-
 // fetch a particular restaurant by its id 
 export async function fetchRestaurantById(
   placeId: string, 
@@ -71,15 +53,3 @@ export async function fetchImageUrls(photoRefs: String[]): Promise<string[]> {
 
   return imageUrls;
 };
-
-export async function fetchDistanceInfo(originLatLng: string, destinationPlaceId: string) {
-  const requestUrl = `${process.env.HOST_URL}/api/distance-matrix?` + 
-    `origins=${origin}` +
-    `&destinations=place_id:${destinationPlaceId}`
-
-  const res = await fetch(requestUrl);
-  if (!res.ok) {
-    throw new Error(`Failed to fetch distance info`);
-  }
-  return res.json();
-}
